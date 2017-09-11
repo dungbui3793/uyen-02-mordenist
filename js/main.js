75fe039aa02f2805574dea7a555944da384276d0
DESKTOP-H184TC7\Vo Huyen Lan Uyen <lanuyen.uit@gmail.com>
2017-09-11 14:12:26 +0700

    slider

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
-$(function () {
-})
+// var $item = $('.carousel .item');
+// $item.eq(0).addClass('active');
+// // $('.carousel .circle').remove();
+// // // $('.carousel ').addClass('contain-courousel');
+//
+// $('.carousel ').remove('.circle');
+// $('.carousel').carousel({
+//     interval: 6000,
+//     pause: "false"
+// });
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true,
+    centerPadding: 0,
+    // autoplay: true
+});
