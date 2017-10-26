bc4a12edf7f9f9e5467050c3d5bc708a210fc302
DESKTOP-H184TC7\Vo Huyen Lan Uyen <lanuyen.uit@gmail.com>
2017-10-26 16:57:17 +0700

    fix 2017/10/26

--- axit/js/main.js
+++ axit/js/main.js
@@ -1,20 +1,18 @@
 $(document).ready(function(){
     $('#nav-icon1').click(function(){
         $(this).toggleClass('open');
+        $("#myNavbar").slideToggle("slow");
     });
-
-
-    $(document).mouseup(function(e) {
-        var container = $("#myNavbar");
-        var container02 =  $("#nav-icon1");
-        // if the target of the click isn't the container nor a descendant of the container
-        if (!container.is(e.target) && container.has(e.target).length === 0 &&
-            !container02.is(e.target) && container02.has(e.target).length === 0) {
-            // container.removeClass("in");
-            $('#nav-icon1').click();
-
-        }
-
-    });
-
+    // $(document).mouseup(function(e) {
+    //     var container = $("#myNavbar");
+    //     var container02 =  $("#nav-icon1");
+    //     // if the target of the click isn't the container nor a descendant of the container
+    //     if (!container.is(e.target) && container.has(e.target).length === 0 &&
+    //         !container02.is(e.target) && container02.has(e.target).length === 0) {
+    //         // container.removeClass("in");
+    //         $('#nav-icon1').click();
+    //
+    //     }
+    //
+    // });
 });
